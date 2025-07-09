26f3fbb6746617ea8fc7f2072e291e11835ee115
Peter Korobeynikov <p.korobeynikov@sirius-sft.ru>
2025-07-09 03:15:46 +0300

    procont-ex: ex - Gateway update

--- lib/openplc_networking.h
+++
@@ -1,8 +0,0 @@
-// ----------------------------------------------------------------------------
-// Copyright (C) 2016-2024: AMBITECS LLC
-// ----------------------------------------------------------------------------
-//client.cpp
-int connect_to_tcp_server(uint8_t *ip_address, uint16_t port, int method);
-int send_tcp_message(uint8_t *msg, size_t msg_size, int socket_id);
-int receive_tcp_message(uint8_t *msg_buffer, size_t buffer_size, int socket_id);
-int close_tcp_connection(int socket_id);
